1bdaae737b3448e3b5120bc39284949343d0e80e
Szymon Pomorski <szymon.pomorski@nocowanie.pl>
2021-09-01 15:51:50 +0200

    acm headers

--- src/callbacks/useApiCallback.js
+++ src/callbacks/useApiCallback.js
@@ -13,7 +13,10 @@ const useApiCallback = (callback, callImmediately = false)  => {
         return await fetch(fetchUrl,
             {
                 headers: {
-                    'Access-Control-Allow-Headers': "*"
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+                    'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
                 }
             })
             .then(response => response.json())
