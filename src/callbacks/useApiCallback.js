6d023ee34a722067907c26346108680a7a704b35
Szymon Pomorski <szymon.pomorski@nocowanie.pl>
2021-09-02 10:03:45 +0200

    fix

--- src/callbacks/useApiCallback.js
+++ src/callbacks/useApiCallback.js
@@ -14,7 +14,6 @@ const useApiCallback = (callback, callImmediately = false)  => {
             {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': 'true',
                     'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
                     'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
                 }
